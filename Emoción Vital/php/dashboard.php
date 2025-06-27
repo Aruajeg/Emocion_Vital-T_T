87b35cbd700a54d6fbca7b59c6a16ac9aec869cb
Angelo Lelli <angelolelli2003@gmail.com>
2025-06-27 16:11:23 -0400

    Update dashboard.php

--- Emoción Vital/php/dashboard.php
+++ Emoción Vital/php/dashboard.php
@@ -57,7 +57,7 @@
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">PRINCIPAL</div>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="/php/dashboard.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 INICIO
                             </a>
