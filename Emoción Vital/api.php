ea719a2a8f2688b83225eb5ac8d405cb3c8f98aa
Aruajeg <Alberth.jesus1990@gmail.com>
2025-07-01 20:15:08 -0400

    Update api.php

--- Emoción Vital/api.php
+++ Emoción Vital/api.php
@@ -5,7 +5,7 @@ require './Services/helper.php';
 
 $action = $_POST['action'];
 
-// SECRET KEY 4/0AVMBsJh6q9aog11cMO8xme6jCLEu1xo-vChxkhwN7eaFkmX6O0RwW7CeNhmbhPreymwLi
+// SECRET KEY 4/0AVMBsJh6q9aog11cMO8xme6jCLEu1xo-vChxkhwN7eaFkmX6O0RwW7CeNhmbhPreymwLiw
 
 $client = getClient();
 $calendarService = new Google_Service_Calendar($client);
