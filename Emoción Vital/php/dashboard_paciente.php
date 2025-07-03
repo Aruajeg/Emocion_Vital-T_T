d05b2fd3994c9df9eca45bd557f3261e05542a42
Aruajeg <Alberth.jesus1990@gmail.com>
2025-07-02 22:35:55 -0400

    Footer

--- Emoción Vital/php/dashboard_paciente.php
+++ Emoción Vital/php/dashboard_paciente.php
@@ -34,7 +34,7 @@ $total_citas = $conn->query("SELECT COUNT(*) FROM solicitar_cita")->fetch_row()[
         <meta name="author" content="" />
         <title>PACIENTE</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-        <link href="/Style/styles_dashboard.css" rel="stylesheet" />
+        <link href="/Style/styles_dashboard_pacientes.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
@@ -43,7 +43,7 @@ $total_citas = $conn->query("SELECT COUNT(*) FROM solicitar_cita")->fetch_row()[
             <button class="btn btn-link btn-lg order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Brand-->
             <img src="/Imagen/Psico4.png" alt="Logo del consultorio" style="height:50px; vertical-align:middle; margin-left:20px;">
-            <a class="navbar-brand ps-3" href="#">EMOCIÓOOOON VITAL</a>
+            <a class="navbar-brand ps-3" href="#">EMOCIÓN VITAL</a>
             <!-- Navbar Search-->
             <!--<form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 <div class="input-group">
@@ -168,10 +168,10 @@ $total_citas = $conn->query("SELECT COUNT(*) FROM solicitar_cita")->fetch_row()[
 </div>
                         
                         
-                     <!--</div>
-                </main>-->
-                <!--<div id="layoutSidenav_footer">
-                    <footer class="sb-footer text-center text-lg-start bg-dark text-muted">-->
+                     </div>
+                </main>
+                <div id="layoutSidenav_footer">
+                    <footer class="papa2">
                         <!-- Section: Social media -->
                         <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
                             <!-- Left -->
@@ -206,7 +206,7 @@ $total_citas = $conn->query("SELECT COUNT(*) FROM solicitar_cita")->fetch_row()[
                         <!-- Section: Social media -->
 
                         <!-- Section: Links  -->
-                        <section class="">
+                        <section class="papa">
                             <div class="row text-left">
                                 <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
                                     <h5 class="text-uppercase">Enlaces útiles</h5>
@@ -269,7 +269,7 @@ $total_citas = $conn->query("SELECT COUNT(*) FROM solicitar_cita")->fetch_row()[
                         <!-- Section: Links  -->
 
                         <!-- Copyright -->
-                        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+                        <div class="text-center p-4" style="background-color: rgb(255, 255, 255);">
                             © 2025 EMOCIÓN VITAL. Todos los derechos reservados.
                         </div>
                         <!-- Copyright -->
